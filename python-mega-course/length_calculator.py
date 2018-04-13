9d4c7fab15ecc200a4be625de49b7e0bcff9d8e1
boko91 <boko91@gmail.com>
2018-04-13 14:08:17 -0700

    Changed location of string conversion str()

--- python-mega-course/length_calculator.py
+++ python-mega-course/length_calculator.py
@@ -1,6 +1,6 @@
 def string_length(mystring):
     return len(mystring)
-user_input = input("Enter a word: ")
-length = (str(string_length(user_input)))
+user_input = str(input("Enter a word: "))
+length = string_length(user_input)
 printcall = ''.join(["Your word is ", length, " characters long."])
 print(printcall)
