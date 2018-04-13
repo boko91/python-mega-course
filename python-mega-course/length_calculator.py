289c9eb160403b8f9ba1459dea4a6106929a4360
boko91 <boko91@gmail.com>
2018-04-13 15:21:37 -0700

    included ascii encoder, try function

--- python-mega-course/length_calculator.py
+++ python-mega-course/length_calculator.py
@@ -1,6 +1,13 @@
 def string_length(mystring):
     return len(mystring)
-user_input = str(input("Enter a word: "))
-length = string_length(user_input)
-printcall = ''.join(["Your word is ", length, " characters long."])
-print(printcall)
+
+user_input = input("Enter a word: ")
+
+try:
+    user_input.encode('ascii')
+except UnicodeEncodeError:
+    print("Bad input!!! You broke the rules. Good bye.")
+else:
+    length = str(string_length(user_input))
+    printcall = ''.join(["Your word is ", length, " characters long."])
+    print(printcall)
